d0830445b39ff37f59e61fe19b916e34f474f833
MOHAMMED_XD <mohammedkab2001@gmail.com>
2024-04-01 02:04:20 +0200

    remove movie store

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="d-md-flex d-md-none px-5 py-4">
-    <v-card color="black" class="ma-auto">
-      <v-img :src="moiveNow?.large_cover_image" class="object-fit-contain">
+    <v-card :loading="isFetchingRating" class="filtered-background ma-auto" :style="{ 'background-image': `url(${moiveNow?.background_image})` }">
+      <v-img :src="moiveNow?.large_cover_image"  class="w-pc ma-auto">
       </v-img>
-      <v-card>
-        <v-tabs v-model="tab" bg-color="primary">
+      <v-card class="w-tab ma-auto ">
+        <v-tabs  v-model="tab" bg-color="primary">
           <v-tab value="one">info</v-tab>
           <v-tab v-if="moiveNow?.yt_trailer_code" value="two">trailers</v-tab>
           <v-tab value="three" v-if="moiveNow?.description_full">about</v-tab>
         </v-tabs>
 
-        <v-card-text class="bg-yellow-400">
+        <v-card-text class="bg-yellow-400 ">
           <v-window v-model="tab">
             <v-window-item value="one">
               <div class="d-flex align-center">
@@ -24,7 +23,7 @@
                 ></v-rating>
                 <p>{{ moiveNow?.rating }}</p>
               </div>
-              <v-row class="ma-auto" >
+              <v-row class="ma-auto " >
                 <v-chip
                 size="small"
                 class="ma-1 pa-2"
@@ -53,7 +52,7 @@
             </v-window-item>
           </v-window>
         </v-card-text>
-        <v-card color="black">
+        <v-card color="black" class="py-8">
           <v-card-title  >Download</v-card-title>
           <v-card-subtitle >Torrent</v-card-subtitle>
           <v-row class="itmes">
@@ -74,25 +73,37 @@
         </v-card>
       </v-card>
     </v-card>
-  </div>
 </template>
 
 <script lang="ts" setup>
-import { useAppStore } from "@/store/movie";
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 import type { Movie } from "@/types/Movie";
 import { useRoute } from "vue-router";
 import { getOrderStatusColor } from "@/helper/color";
+import { movie_details } from "../helper/axios";
+import { useQuery } from "@tanstack/vue-query";
+import { AxiosResponse } from "axios";
 let vueRouter = useRoute();
-let appStore = useAppStore();
 const tab = ref();
-let moiveNow = ref<Movie>();
-onMounted(async () => {
-  moiveNow.value = await appStore.fetchMovie(
-    Number.parseInt(vueRouter.params.id as string)
-  );
 
-});
+const { data: moiveNow, isFetching: isFetchingRating } = useQuery({
+  queryKey: ["Movie",Number.parseInt(vueRouter.params.id as string)],
+  queryFn: async () => {
+
+    const response: AxiosResponse<Movie | any> = await movie_details.get(
+        "/",
+        {
+          params: {
+            movie_id:   Number.parseInt(vueRouter.params.id as string),
+          },
+        }
+      );
+      return response.data.data?.movie;
+ } ,
+  staleTime: Infinity,
+  }
+ );
+
 </script>
 <style scoped>
 .itmes {
@@ -145,6 +156,14 @@ onMounted(async () => {
   background-color: rgba(0, 0, 0, 0.5);
 }
 @media (min-width: 850px) {
+  .w-pc{
+    width: 550px;
+
+
+}
+.w-tab{
+width: 75%;
+}
 }
 .movieTitle {
   font-weight: 800;
@@ -160,5 +179,26 @@ onMounted(async () => {
   color: #333;
   padding: 20px;
 
+}
+.filtered-background {
+  position: relative;
+  color: white;
+}
+
+.filtered-background::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: inherit;
+  filter: brightness(50%);
+  background-repeat: no-repeat;
+  background-size: cover;
+  z-index: -1;
+}
+.filtered-background {
+  position: relative;
 }
 </style>
